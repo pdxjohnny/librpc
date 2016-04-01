23898fbb14e37b77a5c8e7d13870a1b9b366703a
John Andersen <johnandersenpdx@gmail.com>
2016-04-01 07:52:30 +0000

    parse_http is grabing path
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/message.c
+++ src/message.c
@@ -45,12 +45,36 @@ int rpc_message_parse(struct rpc_message * msg, char * buffer, int buffer_size) 
 
 // Parse an http mesage
 int rpc_message_parse_http(struct rpc_message * msg, char * buffer, int buffer_size) {
-    printf("HTTP PARSE %s", buffer);
+    int err;
+
+    // Make a string to hold the path
+    char path[buffer_size + 1];
+    memset(path, 0, sizeof(path));
+    // The path will be right after the HTTP method (GET, POST, HEAD) so we
+    // have to find out where the http method ends and the path begins
+    char * path_start = strchr(buffer, ' ') + 1;
+    // path_start will be NULL if it couldnt find a space in buffer
+    if (path_start == NULL) {
+        errno = EBADMSG;
+        return -1;
+    }
+
+    // Grab the path so from after the method to the next space before HTTP/X.X
+    err = rpc_string_untildelim(path, path_start, buffer_size + 1, ' ');
+    if (err == -1) {
+        return -1;
+    }
+    printf("path \'%s\'\n", path);
+
     return EXIT_SUCCESS;
 }
 
 // Free the message when we are done with it
 int rpc_message_free(struct rpc_message * msg) {
+    if (msg->method != NULL) {
+        free(msg->method);
+        msg->method = NULL;
+    }
     return EXIT_SUCCESS;
 }
 
--- src/string.c
+++ src/string.c
@@ -2,11 +2,11 @@
 #include <librpc.h>
 
 // Fills first with the first section of src, segment is up to delim
-int rpc_string_untildelim(char * dest, char * src, int dest_size, char split_on) {
+int rpc_string_untildelim(char * dest, char * src, int dest_size, char delim) {
     // Pointer the the first delim in src
     char * first_delim = NULL;
     // Check the first segment of the message
-    first_delim = strchr(src, '\n');
+    first_delim = strchr(src, delim);
     // first_delim will be NULL if it couldnt find a delim in the src
     if (first_delim == NULL) {
         errno = EBADMSG;
@@ -16,7 +16,6 @@ int rpc_string_untildelim(char * dest, char * src, int dest_size, char split_on)
     uintptr_t first_segment_length = first_delim - src;
     // We only have a dest of dest_size and we have to NULL terminate dest so
     // it can only by dest_size - 1 long
-    printf("first segment is %ld long\n", first_segment_length);
     if (first_segment_length > (dest_size - 1)) {
         errno = ERANGE;
         return -1;
@@ -24,7 +23,7 @@ int rpc_string_untildelim(char * dest, char * src, int dest_size, char split_on)
     // Copy the first segment from src into its own string
     memcpy(dest, src, first_segment_length);
     // NULL terminate the first segment string
-    dest[first_segment_length + 1] = '\0';
+    dest[first_segment_length] = '\0';
 
     return EXIT_SUCCESS;
 }
