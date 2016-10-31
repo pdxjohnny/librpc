81effc7cea22dd4afa83a6547bde3c2e53c8c99a
John Andersen <johnandersenpdx@gmail.com>
2016-10-31 11:32:31 -0700

    Instead of waiting whoel connection on each flush return from recv
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/message_parse_raw.c
+++ src/message_parse_raw.c
@@ -2,6 +2,9 @@
 
 // Parse an raw mesage
 int rpc_message_parse_raw(struct rpc_message * msg, const char * buffer, int buffer_size) {
+#ifdef RPC_LOGGING
+    printf("rpc_message_parse_raw: got message: \"%s\"\n", buffer);
+#endif
     // Just put everything we got in the buffer
     return rpc_message_append_to_buffer(msg, buffer, buffer_size);
 }
